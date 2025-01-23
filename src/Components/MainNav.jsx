8a141c58e8900312e71cc98c83e24dc7473dffd5
LordAnthrax94 <andreagi1994@gmail.com>
2025-01-23 17:31:35 +0100

    fix: impostata nav

--- src/Components/MainNav.jsx
+++ src/Components/MainNav.jsx
@@ -2,7 +2,19 @@ import { NavLink } from "react-router-dom";
 
 const MainNav = () =>{
   return (
-    
+    <nav className="container">
+      <ul className="nav">
+        <li className="nav-item">
+          <NavLink to="/" className="nav-link">Home</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="About" className="nav-link">Chi Siamo</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="Contacts" className="nav-link">Contatti</NavLink>
+        </li>
+      </ul>
+    </nav>
   )
 }
 
